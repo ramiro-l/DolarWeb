8a8472d2f0c8aede35bbf8372b815b0a04e22b16
Ramiro Lugo <ramideporte@gmail.com>
2022-03-01 22:10:09 -0300

    edit router 4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter} from "react-router-dom";
+//import {HashRouter} from "react-router-dom";
 import ReactDOM from 'react-dom';
 import App from './App';
 
@@ -7,9 +7,9 @@ import App from './App';
 function index() {
   ReactDOM.render(
     <React.StrictMode>
-      <HashRouter>
+
         <App />
-      </HashRouter>
+    
     </React.StrictMode>,
     document.getElementById('root')
   );
